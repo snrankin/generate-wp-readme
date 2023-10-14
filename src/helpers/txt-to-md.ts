3f02c886f661c502da4b4c8c89d0e75194819759
Sam Rankin <samrankin.dev@gmail.com>
2023-10-13 18:42:37 -0700

    feat(txt-to-md.ts): add functionality to convert a txt file to markdown format
    This commit adds a new file `txt-to-md.ts` which contains a function `convertToMd` that converts a txt file to markdown format. The function takes in the path of the txt file, the URL of the screenshot, and the directory where the screenshots are stored.
    
    The `convertToMd` function performs the following tasks:
    - Converts headings in the txt file to markdown headings.
    - Converts a specific section in the txt file that lists contributors to include links to their WordPress.org profiles.
    - Processes screenshots, if any, by collecting the screenshot files from the specified directory and replacing the corresponding text in the txt file with markdown image tags.
    - Formats code blocks in the txt file by adding indentation and a trailing newline.
    
    After the conversion, the resulting markdown content is written to a README.md file in the same directory as the original txt file.
    
    This functionality was added to improve the readability and presentation of txt files by converting them to a more widely used markdown format.

---
+++ src/helpers/txt-to-md.ts
@@ -0,0 +1,110 @@
+import fs from 'fs-extra';
+import { fullPath, fileContents, getProjectSlug, log, StringDictionary, arraysToObject } from './utils';
+import path from 'path';
+import chalk from 'chalk';
+function contributors(readme: string): string {
+	// Include w.org profiles for contributors.
+	const contributorsMatch = readme.match(new RegExp('(\\*\\*Contributors:\\*\\* )(.+)', 'm'));
+
+	if (contributorsMatch && contributorsMatch.length >= 1) {
+		const contributorsSearch = contributorsMatch[0];
+		let contributorsReplace = contributorsMatch[1];
+		const profiles: string[] = [];
+
+		// Fill profiles.
+		contributorsMatch[2].split(',').forEach((value) => {
+			value = value.trim();
+			profiles.push('[' + value + '](https://profiles.wordpress.org/' + value + '/)');
+		});
+
+		contributorsReplace += profiles.join(', ');
+
+		// Add line break.
+		contributorsReplace += '  ';
+
+		readme = readme.replace(contributorsSearch, contributorsReplace);
+	}
+
+	return readme;
+}
+
+function screenshots(readme: string, screenshotUrl: string, screenshotsDir: string): string {
+	// process screenshots, if any
+	const screenshotMatch = readme.match(new RegExp('## Screenshots ##([^#]*)', 'im'));
+	if (screenshotUrl && screenshotMatch && screenshotMatch.length > 1) {
+		const plugin = getProjectSlug();
+
+		const imageDir = fullPath(screenshotsDir);
+
+		const imageDirRel = path.relative(process.cwd(), imageDir);
+
+		// Collect screenshots content
+
+		const screenshotFiles: string[] = [];
+
+		let files = fs.readdirSync(imageDir);
+
+		console.log('🚀 ~ file: txt-to-md.ts:48 ~ screenshots ~ files :', files);
+		if (files.length > 0) {
+			files.forEach(function (file) {
+				screenshotFiles.push(`./${imageDirRel}/${file}`);
+			});
+		}
+
+		// parse screenshot list into array
+		const globalMatch = screenshotMatch[1].match(new RegExp('^[0-9]+\\. (.*)', 'gim'));
+
+		const matchArray: string[] = [];
+		let nonGlobalMatch, i;
+		if (globalMatch != null && globalMatch.length > 0) {
+			globalMatch.forEach((matchItem) => {
+				nonGlobalMatch = matchItem.match(new RegExp('^[0-9]+\\. (.*)', 'im'));
+
+				if (nonGlobalMatch != null && nonGlobalMatch.length > 0) {
+					matchArray.push(nonGlobalMatch[1]);
+				}
+			});
+			const screenshots = arraysToObject(screenshotFiles, matchArray);
+
+			if (matchArray.length > 0) {
+				Object.entries(screenshots).forEach(([screenshot, match]) => {
+					readme = readme.replace(match, '![' + match + '](' + screenshot + ')\n');
+				});
+			}
+		}
+	}
+
+	return readme;
+}
+export function convertToMd(file: string, screenshotUrl: string, screenshotsDir: string) {
+	var fileDir = path.dirname(fullPath(file));
+
+	const md = path.join(fileDir, 'README.md');
+
+	let readme = fileContents(file);
+
+	// Convert Headings.
+
+	readme = readme.replace(new RegExp('^=([^=]+)=*?[\\s ]*?$', 'gim'), '###$1###');
+	readme = readme.replace(new RegExp('^==([^=]+)==*?[\\s ]*?$', 'mig'), '##$1##');
+	readme = readme.replace(new RegExp('^===([^=]+)===*?[\\s ]*?$', 'gim'), '#$1#');
+
+	const headerMatch = readme.match(new RegExp(/([^##]*)(?:\n##|$)/, 'm'));
+	if (headerMatch && headerMatch.length >= 1) {
+		const headerSearch = headerMatch[1];
+		const headerReplace = headerSearch.replace(new RegExp('^([^:\r\n*]{1}[^:\r\n#\\]\\[]+): (.+)', 'gim'), '**$1:** $2  '); // eslint-disable-line
+		readme = readme.replace(headerSearch, headerReplace);
+	}
+
+	readme = contributors(readme);
+	readme = screenshots(readme, screenshotUrl, screenshotsDir);
+	readme = readme.replace(new RegExp('^`$[\n\r]+([^`]*)[\n\r]+^`$', 'gm'), (codeblock, codeblockContents) => {
+		const lines = codeblockContents.split('\n');
+		// Add newline and indent all lines in the codeblock by one tab.
+		return '\n\t' + lines.join('\n\t') + '\n'; // trailing newline is unnecessary but adds some symmetry.
+	});
+
+	fs.writeFileSync(md, readme);
+
+	log(`Generated the file ${chalk.cyan(path.relative(process.cwd(), md))}`, 'success');
+}

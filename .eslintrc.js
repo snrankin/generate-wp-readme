9913e9f11cc1c4a1ee21d9fe1da80118e0d44124
Sam Rankin <samrankin.dev@gmail.com>
2023-10-16 12:37:16 -0700

    chore(eslintrc.js): update eslint configuration to use TypeScript parser and recommended rules
    The eslint configuration file (.eslintrc.js) has been updated to use the TypeScript parser (@typescript-eslint/parser) and recommended rules (plugin:@typescript-eslint/recommended). This change improves the linting process by providing better support for TypeScript syntax and enforcing best practices specific to TypeScript projects.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,13 @@
+/* eslint-env node */
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'prettier'],
-	parser: '@babel/eslint-parser',
-	globals: {
-		document: true,
-		rwp: true,
-		window: true,
-	},
-	env: {
-		es2022: true,
-		browser: true,
-		jquery: true,
-		commonjs: true,
-		node: true,
-	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		ecmaFeatures: {
-			globalReturn: true,
-			generators: false,
-			objectLiteralDuplicateProperties: false,
-			experimentalObjectRestSpread: true,
-		},
-		requireConfigFile: false,
-		ecmaVersion: 2017,
-		allowImportExportEverywhere: true,
-		sourceType: 'module',
+		project: true,
+		tsconfigRootDir: __dirname,
 	},
-	settings: {
-		'import/ignore': ['node_modules', '\\.(coffee|scss|css|less|hbs|svg|json)$'],
-	},
+	plugins: ['@typescript-eslint'],
 	rules: {
 		quotes: ['error', 'single'],
 		'no-async-promise-executor': 0,

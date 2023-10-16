e7977d336b25554a2e3515c7a3461334b0a07d4d
Sam Rankin <samrankin.dev@gmail.com>
2023-10-16 13:44:03 -0700

    chore(release.config.js): update semantic-release configuration
    - Change the casing of the plugins from single quotes to double quotes for consistency.
    - Update the release rules for the commit analyzer plugin to include patch releases for docs related to README and readme files, as well as build related to dependencies.
    - Remove the replace plugin configuration as it is no longer needed.
    - Update the configuration for the release notes generator plugin to use the preset.
    - Remove the changelog plugin configuration as it is no longer needed.
    - Remove the exec plugin configuration for preparing the changelog, as it is no longer needed.
    - Add the changelog plugin configuration to generate the changelog file.
    - Update the github plugin configuration to include a success comment with the version and a link to the GitHub release, and add the releases to the top.
    - Update the npm plugin configuration to disable npm publishing.
    - Remove the exec plugin configurations for preparing the readme, starting the backup, and archiving, as they are no longer needed.
    - Remove the git plugin configuration for committing the release assets, as it is no longer needed.
    - Update the branches configuration to only include the main branch.

--- release.config.js
+++ release.config.js
@@ -9,126 +9,58 @@ console.log(process.env);
 module.exports = {
 	plugins: [
 		[
-			"@semantic-release/commit-analyzer",
+			'@semantic-release/commit-analyzer',
 			{
 				preset,
 				// presetConfig,
 				// parserOpts: parserConfig,
 				releaseRules: [
 					{
-						type: "docs",
-						scope: "*README*",
-						release: "patch",
+						type: 'docs',
+						scope: '*README*',
+						release: 'patch',
 					},
 					{
-						type: "build",
-						scope: "deps",
-						release: "patch",
+						type: 'build',
+						scope: 'deps',
+						release: 'patch',
 					},
 					{
-						type: "docs",
-						scope: "*readme*",
-						release: "patch",
+						type: 'docs',
+						scope: '*readme*',
+						release: 'patch',
 					},
 					{
-						type: "refactor",
-						release: "patch",
+						type: 'refactor',
+						release: 'patch',
 					},
 					{
-						type: "chore",
-						scope: "release",
+						type: 'chore',
+						scope: 'release',
 						release: false,
 					},
 					{
-						scope: "no-release",
+						scope: 'no-release',
 						release: false,
 					},
 				],
 			},
 		],
 		[
-			"@google/semantic-release-replace-plugin",
-			{
-				replacements: [
-					{
-						files: ["hello-riester.php"],
-						from: "Version: .*",
-						to: "Version: ${nextRelease.version}", //eslint-disable-line
-						results: [
-							{
-								file: "hello-riester.php",
-								hasChanged: true,
-								numMatches: 1,
-								numReplacements: 1,
-							},
-						],
-						countMatches: true,
-					},
-				],
-			},
-		],
-		[
-			"@semantic-release/release-notes-generator",
+			'@semantic-release/release-notes-generator',
 			{
 				preset,
-				// presetConfig,
-				// parserOpts: parserConfig,
-				// writerOpts: writerConfig,
 			},
 		],
+		'@semantic-release/changelog'
 		[
-			"@semantic-release/changelog",
+			'@semantic-release/github',
 			{
-				changelogFile: "CHANGELOG.md",
-				changelogTitle: "# Hello RIESTER Changelog\n---",
+				successComment: ':bookmark: Version ${nextRelease.version} is now available on [GitHub release](<github_release_url>)'
+				addReleases: 'top'
 			},
 		],
-		[
-			"@semantic-release/exec",
-			{
-				prepareCmd: "npm run changelog",
-			},
-		],
-		[
-			"@semantic-release/npm",
-			{
-				npmPublish: false,
-			},
-		],
-		[
-			"@semantic-release/exec",
-			{
-				prepareCmd: "npm run readme",
-			},
-		],
-		[
-			"@semantic-release/exec",
-			{
-				prepareCmd: "npm run start-backup",
-			},
-		],
-		[
-			"@semantic-release/exec",
-			{
-				prepareCmd: "npm run archive",
-			},
-		],
-		[
-			"@semantic-release/git",
-			{
-				assets: [
-					"package/hello-riester.zip",
-					"CHANGELOG.md",
-					"package.json",
-					"package-lock.json",
-					"hello-riester.php",
-					"readme.txt",
-					"README.md",
-					"languages/hello-riester.pot",
-				],
-				message: "chore(release): ${nextRelease.version} [skip ci]", //eslint-disable-line
-			},
-		],
+		'@semantic-release/npm',
 	],
-	branches: ["main"],
+	branches: ['main'],
 };
